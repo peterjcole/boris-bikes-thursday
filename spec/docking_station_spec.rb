6dd2641e0fb61e68795a447d17706d70d21914a3
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-05-13 16:11:42 +0100

    all examples passing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,9 @@ describe DockingStation do
   #   expect(DockingStation).to respond_to :release_bike
   # end
   it { should respond_to :release_bike }
+
+  it 'should return a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
