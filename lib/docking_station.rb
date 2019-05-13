b16ec28892f8bf74b2b9987ec181e46624f4dba5
edwardreeve <edreeve+git@gmail.com>
2019-05-13 16:57:21 +0100

    DockingStations now store docked bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,13 @@
-# require_relative 'bike.rb'
+require_relative 'bike.rb'
 
 class DockingStation
+  attr_reader :docked
+
   def release_bike
     return Bike.new
+  end
+
+  def dock(bike)
+    @docked = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,17 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { should respond_to(:dock).with(1).argument }
+
+  it { should respond_to :docked }
+
+  it 'should store a bike that\'s been docked' do
+    my_dock = DockingStation.new
+    pedalo = Bike.new
+    my_dock.dock(pedalo)
+
+    expect(my_dock.docked).to eq(pedalo)
+  end
+
 end
